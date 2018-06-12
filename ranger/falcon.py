79ae2c1f18f20c14696ad611c152ae7b100c0a39
Michael Welford <michael.welford@adelaide.edu.au>
2018-06-12 13:15:53 +0930

    First version of ranger theme.

---
+++ ranger/falcon.py
@@ -0,0 +1,138 @@
+# Falcon ranger colorscheme.
+
+from __future__ import (absolute_import, division, print_function)
+
+from ranger.gui.colorscheme import ColorScheme
+from ranger.gui.color import (
+    cyan, magenta, red, white, default,
+    normal, bold, reverse,
+    default_colors,
+)
+
+class Solarized(ColorScheme):
+    progress_bar_color = 3
+
+    def use(self, context):  # pylint: disable=too-many-branches,too-many-statements
+        fg, bg, attr = default_colors
+
+        if context.reset:
+            return default_colors
+
+        elif context.in_browser:
+            fg = 7
+            if context.selected:
+                attr = reverse
+                fg = 15
+            else:
+                attr = normal
+            if context.empty or context.error:
+                fg = 235
+                bg = 160
+            if context.border:
+                fg = default
+            if context.media:
+                if context.image:
+                    fg = 5
+                else:
+                    fg = 5
+            if context.container:
+                fg = 61
+            if context.directory:
+                fg = 12
+            elif context.executable and not \
+                    any((context.media, context.container,
+                         context.fifo, context.socket)):
+                fg = 15
+                attr |= bold
+            if context.socket:
+                fg = 136
+                bg = 230
+                attr |= bold
+            if context.fifo:
+                fg = 136
+                bg = 230
+                attr |= bold
+            if context.device:
+                fg = 244
+                bg = 230
+                attr |= bold
+            if context.link:
+                fg = 37 if context.good else 160
+                attr |= bold
+                if context.bad:
+                    bg = 235
+            if context.tag_marker and not context.selected:
+                attr |= bold
+                if fg in (red, magenta):
+                    fg = white
+                else:
+                    fg = red
+            if not context.selected and (context.cut or context.copied):
+                fg = 234
+                attr |= bold
+            if context.main_column:
+                if context.selected:
+                    attr |= bold
+                if context.marked:
+                    attr |= bold
+                    bg = 237
+            if context.badinfo:
+                if attr & reverse:
+                    bg = magenta
+                else:
+                    fg = magenta
+
+            if context.inactive_pane:
+                fg = 3
+
+        elif context.in_titlebar:
+            attr |= bold
+            if context.hostname:
+                fg = 16 if context.bad else 249
+                if context.bad:
+                    bg = 166
+            elif context.directory:
+                fg = 253
+            elif context.tab:
+                fg = 0 if context.good else 7
+                bg = 5 if context.good else 237
+            elif context.link:
+                fg = cyan
+
+        elif context.in_statusbar:
+            if context.permissions:
+                if context.good:
+                    fg = 3
+                elif context.bad:
+                    fg = 160
+                    bg = 235
+            if context.marked:
+                attr |= bold | reverse
+                fg = 2
+                bg = 15
+            if context.message:
+                if context.bad:
+                    attr |= bold
+                    fg = 160
+                    bg = 235
+            if context.loaded:
+                bg = self.progress_bar_color
+
+        if context.text:
+            if context.highlight:
+                attr |= reverse
+
+        if context.in_taskview:
+            if context.title:
+                fg = 93
+
+            if context.selected:
+                attr |= reverse
+
+            if context.loaded:
+                if context.selected:
+                    fg = self.progress_bar_color
+                else:
+                    bg = self.progress_bar_color
+
+        return fg, bg, attr
