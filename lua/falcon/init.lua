a369d75e0b388ec5b9e35251023e22c42634ce20
Michael Welford <mail@michaelwelford.com>
2022-10-19 21:11:36 +1030

    Pull colours into palette.

--- lua/falcon/init.lua
+++ lua/falcon/init.lua
@@ -1,35 +1,35 @@
 local lush = require('lush')
+local colours = require('falcon.colours')
 local base = require('falcon.base')
 local plugins = require('falcon.plugins')
 local filetypes = require('falcon.filetypes')
 
--- TODO pull this from the colours
 vim.api.nvim_set_var('falcon.palette', {
-  red = '#ff3600',
-  orange = '#ff761a',
-  yellow = '#ffc552',
-  green = '#718e3f',
-  blue_gray = '#99a4bc',
-  purple = '#635196',
-  indigo = '#5521d9',
-  status = '#28282d',
-  status_2 = '#36363a',
-  inactive_status = '#1c1c22',
-  black = '#000004',
-  white = '#F8F8FF',
-  light_gray = '#dfdfe5',
-  normal_gray = '#b4b4b9',
-  mid_gray = '#787882',
-  mid_dark_gray = '#57575e',
-  dark_gray = '#36363a',
-  modified = '#c8d0e3',
-  branch = '#99a4bc',
-  method = '#a1968a',
-  path = '#787882',
-  info = '#787882',
-  hint = '#847b73',
-  error = '#9e1e00',
-  warning = '#bc8f3f'
+  red             = colours.red.hex,
+  orange          = colours.orange.hex,
+  yellow          = colours.yellow.hex,
+  green           = colours.green.hex,
+  blue_gray       = colours.blue_gray.hex,
+  purple          = colours.purple.hex,
+  indigo          = colours.indigo.hex,
+  status          = colours.status.hex,
+  status_2        = colours.dark_gray.hex,
+  inactive_status = colours.inactive_status.hex,
+  black           = colours.black.hex,
+  white           = colours.white.hex,
+  light_gray      = colours.light_gray.hex,
+  normal_gray     = colours.normal_gray.hex,
+  mid_gray        = colours.mid_gray.hex,
+  mid_dark_gray   = colours.mid_dark_gray.hex,
+  dark_gray       = colours.dark_gray.hex,
+  modified        = colours.light_blue_gray.hex,
+  branch          = colours.blue_gray.hex,
+  method          = '#a1968a',
+  path            = colours.mid_gray.hex,
+  info            = colours.mid_gray.hex,
+  hint            = colours.dark_tan.hex,
+  error           = colours.mid_red.hex,
+  warning         = colours.mid_yellow.hex,
 })
 
 local function setup()
