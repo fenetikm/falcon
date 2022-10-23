2b6ac0130854c3bf626e29f63d876dde384b6b3a
Michael Welford <mail@michaelwelford.com>
2022-10-23 11:42:55 +1030

    Add in missing lsp highlights.

--- lua/falcon/extras/lsp.lua
+++ lua/falcon/extras/lsp.lua
@@ -4,40 +4,36 @@ local styles = require('falcon.styles')
 
 local p = lush(function()
     return {
-        LspDiagnosticsDefaultError           {fg = colours.mid_red},
-        LspDiagnosticsDefaultWarning         {fg = colours.mid_yellow},
-        LspDiagnosticsDefaultInformation     {fg = colours.mid_gray},
-        LspDiagnosticsDefaultHint            {fg = colours.dark_tan},
-        LspReferenceRead                     {gui = styles.underline},
-        LspReferenceText                     {gui = styles.underline},
-        LspReferenceWrite                    {gui = styles.underline},
-        LspDiagnosticsVirtualTextError       {fg = colours.mid_red, gui = styles.italic},
-        LspDiagnosticsVirtualTextWarning     {fg = colours.mid_yellow, gui = styles.italic},
-        LspDiagnosticsVirtualTextHint        {fg = colours.dark_tan, gui = styles.italic},
-        LspDiagnosticsVirtualTextInformation {fg = colours.mid_gray, gui = styles.italic},
-        LspDiagnosticsUnderlineError         {gui = styles.undercurl, guisp = colours.mid_red},
-        LspDiagnosticsUnderlineWarning       {gui = styles.undercurl, guisp = colours.mid_yellow},
-        LspDiagnosticsUnderlineHint          {gui = styles.undercurl, guisp = colours.dark_tan},
-        LspDiagnosticsUnderlineInformation   {gui = styles.undercurl, guisp = colours.mid_gray},
-        DiagnosticSignError                  {fg = colours.mid_red},
-        DiagnosticSignWarn                   {fg = colours.mid_yellow},
-        DiagnosticSignInfo                   {fg = colours.mid_gray},
-        DiagnosticSignHint                   {fg = colours.dark_tan},
-        DiagnosticError                      {fg = colours.mid_red},
-        DiagnosticWarn                       {fg = colours.mid_yellow},
-        DiagnosticInfo                       {fg = colours.mid_gray},
-        DiagnosticHint                       {fg = colours.dark_tan},
-        ReferenceRead                        {gui = styles.underline},
-        ReferenceText                        {gui = styles.underline},
-        ReferenceWrite                       {gui = styles.underline},
-        DiagnosticVirtualTextError           {fg = colours.mid_red, gui = styles.italic},
-        DiagnosticVirtualTextWarn            {fg = colours.mid_yellow, gui = styles.italic},
-        DiagnosticVirtualTextHint            {fg = colours.dark_tan, gui = styles.italic},
-        DiagnosticVirtualTextInfo            {fg = colours.mid_gray, gui = styles.italic},
-        DiagnosticUnderlineError             {gui = styles.undercurl, guisp = colours.mid_red},
-        DiagnosticUnderlineWarn              {gui = styles.undercurl, guisp = colours.mid_yellow},
-        DiagnosticUnderlineHint              {gui = styles.undercurl, guisp = colours.dark_tan},
-        DiagnosticUnderlineInfo              {gui = styles.undercurl, guisp = colours.mid_gray},
+        LspCodeLens                 { } , -- Used to color the virtual text of the codelens. See |nvim_buf_set_extmark()|.
+        LspCodeLensSeparator        { } , -- Used to color the seperator between two or more code lens.
+        LspReferenceRead            {gui = styles.underline},
+        LspReferenceText            {gui = styles.underline},
+        LspReferenceWrite           {gui = styles.underline},
+        LspSignatureActiveParameter { } , -- Used to highlight the active parameter in the signature help. See |vim.lsp.handlers.signature_help()|.
+
+        DiagnosticError             {fg = colours.mid_red},
+        DiagnosticHint              {fg = colours.dark_tan},
+        DiagnosticInfo              {fg = colours.mid_gray},
+        DiagnosticWarn              {fg = colours.mid_yellow},
+        DiagnosticFloatingError     { } , -- Used to color "Error" diagnostic messages in diagnostics float. See |vim.diagnostic.open_float()|
+        DiagnosticFloatingHint      { } , -- Used to color "Hint" diagnostic messages in diagnostics float.
+        DiagnosticFloatingInfo      { } , -- Used to color "Info" diagnostic messages in diagnostics float.
+        DiagnosticFloatingWarn      { } , -- Used to color "Warn" diagnostic messages in diagnostics float.
+        DiagnosticSignError         {fg = colours.mid_red},
+        DiagnosticSignHint          {fg = colours.dark_tan},
+        DiagnosticSignInfo          {fg = colours.mid_gray},
+        DiagnosticSignWarn          {fg = colours.mid_yellow},
+        DiagnosticUnderlineError    {gui = styles.undercurl, guisp = colours.mid_red},
+        DiagnosticUnderlineHint     {gui = styles.undercurl, guisp = colours.dark_tan},
+        DiagnosticUnderlineInfo     {gui = styles.undercurl, guisp = colours.mid_gray},
+        DiagnosticUnderlineWarn     {gui = styles.undercurl, guisp = colours.mid_yellow},
+        DiagnosticVirtualTextError  {fg = colours.mid_red, gui = styles.italic},
+        DiagnosticVirtualTextHint   {fg = colours.dark_tan, gui = styles.italic},
+        DiagnosticVirtualTextInfo   {fg = colours.mid_gray, gui = styles.italic},
+        DiagnosticVirtualTextWarn   {fg = colours.mid_yellow, gui = styles.italic},
+        ReferenceRead               {gui = styles.underline},
+        ReferenceText               {gui = styles.underline},
+        ReferenceWrite              {gui = styles.underline},
     }
 end)
 
