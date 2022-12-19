f1ac1cddb3646f89dd6e69cddda9d7dcd55fe4ad
Michael Welford <mail@michaelwelford.com>
2022-12-19 19:16:30 +1030

    Tweak zen telescope.

--- lua/falcon/plugins/zen/telescope.lua
+++ lua/falcon/plugins/zen/telescope.lua
@@ -6,17 +6,17 @@ local p = lush(function()
     return {
         TelescopeBorder         {fg = colours.dark_gray, bg = colours.blue_dark_float},
         TelescopeTitle          {fg = colours.mid_gray_alt, bg = colours.blue_dark_float},
-        TelescopeMatching       {fg = colours.light_gray, bg = colours.blue_dark_float, gui = styles.bold},
+        TelescopeMatching       {fg = colours.normal_gray, bg = colours.blue_dark_float, gui = styles.bold},
         TelescopePromptPrefix   {fg = colours.mid_gray_alt2, bg = colours.blue_dark_float},
         TelescopeSelection      {fg = colours.normal_gray, bg = colours.blue_dark_float},
         TelescopeSelectionCaret {fg = colours.darker_tanner, bg = colours.blue_dark_float},
         TelescopeMultiSelection {fg = colours.normal_gray, bg = colours.blue_dark_float, gui = styles.bold},
-        TelescopeMultiIcon      {fg = colours.normal_gray, bg = colours.blue_dark_float},
+        TelescopeMultiIcon      {fg = colours.normal_gray},
         TelescopeNormal         {fg = colours.mid_gray, bg = colours.blue_dark_float},
         TelescopePreviewNormal  {TelescopeNormal},
         TelescopePromptNormal   {TelescopeNormal},
         TelescopeResultsNormal  {TelescopeNormal},
-        TelescopePromptCounter  {fg = colours.blue_gray, bg = colours.blue_dark_float},
+        TelescopePromptCounter  {fg = colours.blue_gray},
     }
 end)
 
