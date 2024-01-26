2b4137274bfe4f4646206549ac8f8223ed72e4ce
Michael Welford <mail@michaelwelford.com>
2024-01-26 15:52:54 +1030

    Neogit tweaking.

--- lua/falcon/plugins/modern/neogit.lua
+++ lua/falcon/plugins/modern/neogit.lua
@@ -3,17 +3,19 @@ local colours = require('falcon.colours')
 local modern = require('falcon.modern')
 local gitsigns = require('falcon.plugins.common.gitsigns')
 local styles = require('falcon.styles')
+local diff = require('falcon.extras.common.diff')
 
 local p = lush(function()
     return {
         NeogitSectionHeader {modern.Keyword},
+        NeogitUnmergedInto {NeogitSectionHeader},
         NeogitBranch {modern.Normal},
         NeogitBranchHead {},
         NeogitRemote {modern.Normal},
         NeogitTagName {modern.Normal},
         NeogitTagDistance {modern.Comment},
-        NeogitHunkHeader {bg = colours.normal_gray},
-        NeogitHunkHeaderHighlight {bg = colours.bg},
+        NeogitHunkHeader {modern.Comment},
+        NeogitHunkHeaderHighlight {modern.Comment},
         NeogitChangeModified {gitsigns.GitSignsChange},
         NeogitChangeBothModified {NeogitChangeModified},
         NeogitChangeUpdated {NeogitChangeModified},
@@ -21,7 +23,11 @@ local p = lush(function()
         NeogitChangeCopied {NeogitChangeAdded},
         NeogitChangeRenamed {NeogitChangeAdded},
         NeogitChangeNewFile {NeogitChangeAdded},
-        NeogitChangeDeleted {gitsigns.GitSignsDelete}
+        NeogitChangeDeleted {gitsigns.GitSignsDelete},
+        NeogitDiffDelete {diff.diffRemoved},
+        NeogitDiffAdd {diff.diffAdded},
+        NeogitDiffDeleteHighlight {diff.diffRemoved},
+        NeogitDiffAddHighlight {diff.diffAdded},
     }
 end)
 
