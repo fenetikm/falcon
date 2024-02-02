0210e4924ae10642e9b3aa42ccfe9834c6de342a
Michael Welford <mail@michaelwelford.com>
2024-02-02 11:04:34 +1030

    More neogit tweaks.

--- lua/falcon/plugins/modern/neogit.lua
+++ lua/falcon/plugins/modern/neogit.lua
@@ -6,29 +6,33 @@ local styles = require('falcon.styles')
 local diff = require('falcon.extras.common.diff')
 
 local p = lush(function()
-    return {
-        NeogitSectionHeader {modern.Keyword},
-        NeogitUnmergedInto {NeogitSectionHeader},
-        NeogitBranch {modern.Normal},
-        NeogitBranchHead {},
-        NeogitRemote {modern.Normal},
-        NeogitTagName {modern.Normal},
-        NeogitTagDistance {modern.Comment},
-        NeogitHunkHeader {modern.Comment},
-        NeogitHunkHeaderHighlight {modern.Comment},
-        NeogitChangeModified {gitsigns.GitSignsChange},
-        NeogitChangeBothModified {NeogitChangeModified},
-        NeogitChangeUpdated {NeogitChangeModified},
-        NeogitChangeAdded {gitsigns.GitSignsAdd},
-        NeogitChangeCopied {NeogitChangeAdded},
-        NeogitChangeRenamed {NeogitChangeAdded},
-        NeogitChangeNewFile {NeogitChangeAdded},
-        NeogitChangeDeleted {gitsigns.GitSignsDelete},
-        NeogitDiffDelete {diff.diffRemoved},
-        NeogitDiffAdd {diff.diffAdded},
-        NeogitDiffDeleteHighlight {diff.diffRemoved},
-        NeogitDiffAddHighlight {diff.diffAdded},
-    }
+  return {
+    NeogitSectionHeader {modern.Keyword},
+    NeogitUnmergedInto {NeogitSectionHeader},
+    NeogitBranch {modern.Normal},
+    NeogitBranchHead {modern.Normal},
+    NeogitRemote {modern.Normal},
+    NeogitTagName {modern.Normal},
+    NeogitTagDistance {modern.Comment},
+    NeogitHunkHeader {modern.Comment},
+    NeogitHunkHeaderHighlight {modern.Comment},
+    NeogitChangeModified {gitsigns.GitSignsChange},
+    NeogitChangeBothModified {NeogitChangeModified},
+    NeogitChangeUpdated {NeogitChangeModified},
+    NeogitChangeAdded {gitsigns.GitSignsAdd},
+    NeogitChangeCopied {NeogitChangeAdded},
+    NeogitChangeRenamed {NeogitChangeAdded},
+    NeogitChangeNewFile {NeogitChangeAdded},
+    NeogitChangeDeleted {gitsigns.GitSignsDelete},
+    NeogitDiffDelete {diff.diffRemoved},
+    NeogitDiffAdd {diff.diffAdded},
+    NeogitDiffDeleteHighlight {diff.diffRemoved},
+    NeogitDiffAddHighlight {diff.diffAdded},
+    NeogitCommitViewHeader {modern.Keyword},
+    NeogitFilePath {modern.Normal},
+    NeogitDiffHeader {modern.Normal, gui = styles.bold},
+    NeogitDiffHeaderHighlight {modern.MatchParen, gui = styles.bold},
+  }
 end)
 
 return p
