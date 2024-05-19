b29dd38180d036c81d3d2f25b6ce89c9c5d07bf8
Michael Welford <mail@michaelwelford.com>
2024-05-19 18:47:19 +0930

    More dap UI stuffs.

--- lua/falcon/plugins/modern/dapui.lua
+++ lua/falcon/plugins/modern/dapui.lua
@@ -1,11 +1,31 @@
 local lush = require('lush')
 local colours = require('falcon.colours')
+local modern = require('falcon.modern')
+local styles = require('falcon.styles')
 local base_ui = require('falcon.base_ui')
 
 local p = lush(function()
     return {
-        DapUIVariable          {fg = colours.blue_gray},
-        DapUIValue             {fg = colours.red},
+        DapUIVariable               { fg = colours.blue_gray},
+        DapUIValue                  { modern.Normal},
+        DapUIModifiedValue          { modern.Normal},
+        DapUIType                   { modern.Keyword},
+        DapUIScope                  { modern.Keyword},
+        DapUILineNumber             { modern.Normal},
+        DapUISource                 { modern.Normal},
+        DapUIDecoration             { fg = colours.blue_gray},
+        DapUIThread                 { modern.String},
+        DapUIStoppedThread          { modern.Comment},
+        DapUIBreakpointsPath        { modern.Keyword},
+        DapUIBreakpointsCurrentLine { modern.IncSearch},
+        DapUIPlayPause              { fg = colours.green},
+        DapUIRestart                { DapUIPlayPause},
+        DapUIStop                   { fg = colours.red},
+        DapUIStepOver               { fg = colours.dark_tan},
+        DapUIStepInto               { DapUIStepOver},
+        DapUIStepBack               { DapUIStepOver},
+        DapUIStepOut                { DapUIStepOver},
+        DapUIWatchesEmpty           { modern.Normal},
     }
 end)
 
