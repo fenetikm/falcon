52be3cbc0d7a3a51f736e855247a9b105fbb54a2
Michael Welford <mail@michaelwelford.com>
2024-06-21 15:06:23 +0930

    Add in Cursearch, more zsh styles.

--- zsh/falcon.zsh
+++ zsh/falcon.zsh
@@ -18,3 +18,30 @@ man() {
     LESS_TERMCAP_us=$'\e[04;94m' \
     command man "$@"
 }
+
+ZSH_HIGHLIGHT_STYLES[bracket-level-1]='fg=#dfdfe5,bold'
+ZSH_HIGHLIGHT_STYLES[bracket-level-2]='fg=#bfdaff,bold'
+ZSH_HIGHLIGHT_STYLES[bracket-level-3]='fg=#f0e1ce,bold'
+ZSH_HIGHLIGHT_STYLES[bracket-level-4]='fg=#eaffc0,bold'
+
+# see https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/docs/highlighters/main.md
+# what works:
+# - bold
+# - underline
+# - standout (reverse?)
+# - no italic :(
+ZSH_HIGHLIGHT_STYLES[unknown-token]='fg=#4f4f59'
+ZSH_HIGHLIGHT_STYLES[command]='fg=#a1a1a9'
+ZSH_HIGHLIGHT_STYLES[builtin]='fg=#a1a1a9'
+ZSH_HIGHLIGHT_STYLES[function]='fg=#a1a1a9'
+ZSH_HIGHLIGHT_STYLES[alias]='fg=#a1a1a9'
+ZSH_HIGHLIGHT_STYLES[default]='fg=#a1a1a9'
+ZSH_HIGHLIGHT_STYLES[arg0]='fg=#a1a1a9'
+ZSH_HIGHLIGHT_STYLES[reserved-word]='fg=#a1a1a9'
+ZSH_HIGHLIGHT_STYLES[redirection]='fg=#847161'
+ZSH_HIGHLIGHT_STYLES[precommand]='fg=#a1a1a9,underline' #e.g. exec, builtin
+ZSH_HIGHLIGHT_STYLES[hashed-command]='fg=#a1a1a9,underline' #e.g. exec, builtin
+ZSH_HIGHLIGHT_STYLES[autodirectory]='fg=#99a4bc,standout' #e.g. exec, builtin
+
+# todo: contrast this with unknown
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=#787882'
