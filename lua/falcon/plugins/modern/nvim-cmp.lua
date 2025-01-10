b2b78e4f4a9f7cef11c3e80b1adf903e713f55aa
Michael Welford <mail@michaelwelford.com>
2025-01-10 13:10:54 +1030

    Update nvim cmp menu bits.

--- lua/falcon/plugins/modern/nvim-cmp.lua
+++ lua/falcon/plugins/modern/nvim-cmp.lua
@@ -1,9 +1,12 @@
 local lush = require('lush')
 local colours = require('falcon.colours')
+local modern = require('falcon.modern')
 
 local p = lush(function()
     return {
         CmpGhostText {fg = colours.darker_bluer_gray.darken(25)},
+        CmpItemKind {modern.Comment},
+        CmpItemMenu {modern.Comment},
     }
 end)
 
