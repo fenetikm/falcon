620856274427b627453e393e65513b0cf1b4b04c
Michael Welford <mail@michaelwelford.com>
2025-07-28 08:59:42 +0930

    Tweak treesitter context again, again.

--- lua/falcon/plugins/modern/context.lua
+++ lua/falcon/plugins/modern/context.lua
@@ -3,12 +3,12 @@ local colours = require('falcon.colours')
 local styles = require('falcon.styles')
 
 local p = lush(function()
-    return {
-        TreesitterContext {fg = colours.mid_gray_alt, bg = 'NONE'},
-        TreesitterContextLineNumber {fg = colours.mid_gray_alt, bg = 'NONE'},
-        TreesitterContextBottom {fg = colours.mid_gray_alt, bg = 'NONE', gui = styles.underline},
-
-    }
+  return {
+    TreesitterContext { fg = colours.mid_gray_alt, bg = 'NONE' },
+    TreesitterContextLineNumber { fg = colours.mid_gray_alt, bg = 'NONE' },
+    TreesitterContextBottom { fg = colours.mid_gray_alt, bg = 'NONE', gui = styles.underline },
+    TreesitterContextSeparator { fg = colours.mid_gray_alt2, bg = 'NONE' },
+  }
 end)
 
 return p
