d1debc0e393a8937b4a073a4ddcc632743f96076
Michael Welford <mail@michaelwelford.com>
2025-08-04 10:08:40 +0930

    Update context again.

--- lua/falcon/plugins/modern/context.lua
+++ lua/falcon/plugins/modern/context.lua
@@ -1,13 +1,14 @@
 local lush = require('lush')
 local colours = require('falcon.colours')
 local styles = require('falcon.styles')
+local modern = require('falcon.modern')
 
 local p = lush(function()
   return {
-    TreesitterContext { fg = colours.mid_gray_alt, bg = 'NONE' },
-    TreesitterContextLineNumber { fg = colours.mid_gray_alt, bg = 'NONE' },
-    TreesitterContextBottom { fg = colours.mid_gray_alt, bg = 'NONE', gui = styles.underline },
-    TreesitterContextSeparator { fg = colours.mid_gray_alt2, bg = 'NONE' },
+    TreesitterContext { fg = modern.Comment.fg, bg = 'NONE' },
+    TreesitterContextLineNumber { fg = modern.Comment.fg, bg = 'NONE' },
+    TreesitterContextBottom { fg = modern.Comment.fg, bg = 'NONE', gui = styles.underline },
+    TreesitterContextSeparator { fg = modern.Normal.fg, bg = 'NONE' },
   }
 end)
 
