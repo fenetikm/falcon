c1e5253814c51e3d611d3925950cad1a0c67318c
Michael Welford <mail@michaelwelford.com>
2026-05-28 09:49:31 +0930

    chore(diff): better contrast on inline diffs

--- lua/falcon/extras/common/diff.lua
+++ lua/falcon/extras/common/diff.lua
@@ -5,9 +5,9 @@ local base_ui = require('falcon.base_ui')
 local p = lush(function()
   return {
     diffAdded { base_ui.DiffAdd },
-    diffAddedInline { bg = colours.off_green_alt },
+    diffAddedInline { fg = colours.light_gray, bg = colours.off_green_alt },
     diffRemoved { base_ui.DiffDelete },
-    diffRemovedInline { bg = colours.off_red_alt },
+    diffRemovedInline { fg = colours.light_gray, bg = colours.off_red_alt },
     diffFile { base_ui.Title },
     diffLine { base_ui.DiffChange },
     diffSubname { fg = colours.light_gray },
